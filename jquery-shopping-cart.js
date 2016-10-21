6be4f2f966b4a30fd29d248bfefc88af638a89ca
allencwwong <allencwwong@gmail.com>
2016-10-20 21:25:21 -0700

    added addtocart, list menu features to demo

--- jquery-shopping-cart.js
+++ jquery-shopping-cart.js
@@ -1,14 +1,88 @@
-   $.ajax({
-      url: 'http://cmtepxress.com/projects/jsc/items.json',
-      data: 'json',
-      success: function(data) {
-      	console.log(data);
-      		for (item in data){
-      			var $new_item = "<div class='item'><div class='title'>" + data[item].name + "</div>" +
-      			"<div class='price'>" + data[item].price + "</div>" +
-      			"<div class='category'>" + data[item].category + "</div></div>";	
- 				$("#menu").append($new_item);
-      		} 
-      },
-      type: 'GET'
-   });
+$(document).ready(function(){
+
+var items = 
+    [{
+      "id":1,
+      "name":"egg rolls",
+      "price": 5.99,
+      "description": "desc"
+    },
+    {
+      "id":2,
+      "name":"fired rice",
+      "price": 8.99,
+      "description": "desc"
+    },    
+    {
+      "id":3,
+      "name":"chicken fired rice",
+      "price": 11.99,
+      "description": "desc"
+    },
+    {
+      "id":4,
+      "name":"fin soup",
+      "price": 18.95,
+      "description": "desc"
+    }
+    ];
+
+ var cartItems = {};   
+ var total = 0;
+
+  function listMenu(){
+    $('#menu h1').after(function(){
+      var list = "";
+      items.forEach(function(item){
+        list+= "<li><a class='item' data-item-id='"+item.id+"''><div>Name: "+item.name+"</div><div>Price: "+item.price+"</div><div>desc: "+item.description+"</div></a></li>"
+      });
+      return list;
+    });
+  }   
+
+  function removeFromCart(){
+    alert("allen");
+    var itemID = $(this).data('item-id');
+    console.log(itemID);
+    $(this).remove();
+    if(cartItems[itemID]-- <= 0){
+      delete cartItems[itemID] 
+    }else {
+      cartItems[itemID]--;
+    }
+     $('#cart #total').text(totalCalc());   
+    
+  }  
+
+  function addToCart(){
+    var itemID = $(this).data('item-id');
+    console.log(itemID);
+      
+    var cartItem = "<li data-item-id='"+itemID+"'><div>Name: "+items[itemID-1].name+"</div><div>Price: "+items[itemID-1].price+"</div><a onclick='removeFromCart()' class='remove'><div>remove</div></a></li>";
+    
+    //};
+    if(cartItems[itemID]){
+      cartItems[itemID]++;
+    //$('#cart #total').text(totalCalc());      
+    }else{
+      cartItems[itemID] = 1;
+    $('#cart h1').after(cartItem);
+    }
+    $('#cart #total').text(totalCalc());
+    console.log(cartItems);
+
+  }
+
+  function totalCalc(){
+    for(var cartItem in cartItems ){
+      total +=  items[cartItem-1].price * cartItems[cartItem];
+    }
+    return total;
+  }
+
+  listMenu();
+
+  $('.item').on('click',addToCart);
+  // $('.remove').on('click',removeFromCart);
+
+});   
