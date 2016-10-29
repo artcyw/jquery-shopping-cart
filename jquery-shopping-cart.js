e9195694c1f1dbc9ce29cad8465869b9e117cf68
allencwwong <allencwwong@gmail.com>
2016-10-29 13:22:17 -0700

    updated demo

--- jquery-shopping-cart.js
+++ jquery-shopping-cart.js
@@ -27,8 +27,20 @@ var items =
     }
     ];
 
- var cartItems = {};   
- var total = 0;
+
+var cart = {
+  totalPrice: 0,
+  totalQty: 0,
+  items: [],
+  addToCart: function(){},
+  removeFromCart: function(){},
+  getTotal: function(){}
+};
+
+
+
+ // var cartItems = {};   
+ // var total = 0;
 
   function listMenu(){
     $('#menu h1').after(function(){
@@ -40,61 +52,62 @@ var items =
     });
   }   
 
-  function removeFromCart(){
- 
-  //   var itemID = itemID;
-    //$('.remove').on('click',function(){
-      var itemID = $(this).data('item-id');
-  //     //console.log($(this).parent());
-      console.log("iid "+itemID);
-      console.log("ci "+cartItems);
-      console.log("ci[id] "+cartItems[itemID]);
-      if(cartItems[itemID]-1 <= 0){
-        $(this).parent().remove();
-        delete cartItems[itemID]; 
-      }else {
-        cartItems[itemID]--;
-      }
-       $('#cart #total').text(totalCalc());   
-   // });
-  }  
-
-  function addToCart(itemID){
-    //var itemID = arg.data('item-id');
-    console.log(itemID);
-      
-    var cartItem = "<li id='item-"+itemID+"'><div>Name: "+items[itemID-1].name+"</div><div>Price: "+items[itemID-1].price+"</div><a class='remove' data-item-id='"+itemID+"'><div>remove</div></a></li>";
-    
-    //};
-    if(cartItems[itemID]){
-      cartItems[itemID]++;
-    //$('#cart #total').text(totalCalc());      
-    }else{
-      cartItems[itemID] = 1;
-    $('#cart h1').after(cartItem);
-    }
-    $('#cart #total').text(totalCalc());
-    console.log(cartItems);
-    //removeFromCart();
-  }
-
-  function totalCalc(){
-    var temp =0;
-    for(var cartItem in cartItems ){
-      temp +=  items[cartItem-1].price * cartItems[cartItem];
-    }
-    total = temp;
-    return total;
-  }
-
   listMenu();
+  
+ //  function removeFromCart(){
+ 
+ //  //   var itemID = itemID;
+ //    //$('.remove').on('click',function(){
+ //      var itemID = $(this).data('item-id');
+ //  //     //console.log($(this).parent());
+ //      console.log("iid "+itemID);
+ //      console.log("ci "+cartItems);
+ //      console.log("ci[id] "+cartItems[itemID]);
+ //      if(cartItems[itemID]-1 <= 0){
+ //        $(this).parent().remove();
+ //        delete cartItems[itemID]; 
+ //      }else {
+ //        cartItems[itemID]--;
+ //      }
+ //       $('#cart #total').text(totalCalc());   
+ //   // });
+ //  }  
 
-  $('.item').on('click',function(){
-    var itemID = $(this).data('item-id');
-    addToCart(itemID);
-  });
+ //  function addToCart(itemID){
+ //    //var itemID = arg.data('item-id');
+ //    console.log(itemID);
+      
+ //    var cartItem = "<li id='item-"+itemID+"'><div>Name: "+items[itemID-1].name+"</div><div>Price: "+items[itemID-1].price+"</div><a class='remove' data-item-id='"+itemID+"'><div>remove</div></a></li>";
+    
+ //    //};
+ //    if(cartItems[itemID]){
+ //      cartItems[itemID]++;
+ //    //$('#cart #total').text(totalCalc());      
+ //    }else{
+ //      cartItems[itemID] = 1;
+ //    $('#cart h1').after(cartItem);
+ //    }
+ //    $('#cart #total').text(totalCalc());
+ //    console.log(cartItems);
+ //    //removeFromCart();
+ //  }
 
-      $(document).on('click','.remove',removeFromCart);
+ //  function totalCalc(){
+ //    var temp =0;
+ //    for(var cartItem in cartItems ){
+ //      temp +=  items[cartItem-1].price * cartItems[cartItem];
+ //    }
+ //    total = temp;
+ //    return total;
+ //  }
+
+
+ //  $('.item').on('click',function(){
+ //    var itemID = $(this).data('item-id');
+ //    addToCart(itemID);
+ //  });
+
+ //      $(document).on('click','.remove',removeFromCart);
 
   // $('.item').on('click',function(){
 
